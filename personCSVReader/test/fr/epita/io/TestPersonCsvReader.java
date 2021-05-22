1cee8488bd815e4bb57a583240d415a33ad91b2c
thomas <thomas.broussard@local.local>
2021-05-22 10:54:16 +0200

    * computing distribution + sort

--- personCSVReader/test/fr/epita/io/TestPersonCsvReader.java
+++ personCSVReader/test/fr/epita/io/TestPersonCsvReader.java
@@ -48,13 +48,31 @@ public class TestPersonCsvReader {
 		//have the distribution
 
 		System.out.println("printing out the distribution across ages");
-		persons.sort(Comparator.comparingInt(Person::getAge));
+	//	persons.sort(Comparator.comparingInt(Person::getAge));
+
 		for (Person person : persons){
 			String ageAsString = String.valueOf(person.getAge());
 			Integer count = distributionMap.computeIfAbsent(ageAsString, sexValue -> 0) + 1 ;
 			distributionMap.put(ageAsString, count);
 		}
 		System.out.println(distributionMap);
+		//option to sort on the map
+		Set<String> strings = distributionMap.keySet();
+		List<String> sortedKeys = new ArrayList<>(strings);
+		sortedKeys.sort(Comparator.comparing(String::toString));
+		sortedKeys.forEach(s -> {
+			System.out.println(s + " : " + distributionMap.get(s));
+		});
+
+
+		System.out.println("displaying result sorted on values");
+		Set<Map.Entry<String, Integer>> e = distributionMap.entrySet();
+		List<Map.Entry<String, Integer>> sortedEntries = new ArrayList<>(e);
+		sortedEntries.sort((pair1,pair2)-> pair2.getValue().compareTo(pair1.getValue()));
+		sortedEntries.forEach( pair -> 	System.out.println(pair.getKey() + " : " + pair.getValue()));
+
+
+
 
 
 
